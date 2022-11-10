f085d59a4873a56a20895880a34982adb96e12c9
Codymo511 <55954712+Codymo511@users.noreply.github.com>
2022-11-10 17:31:34 -0500

    Updated methods

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,6 @@ ActiveRecord::Schema.define(version: 2022_10_21_155319) do
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.string "token"
-    t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
